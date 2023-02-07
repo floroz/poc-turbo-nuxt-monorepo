d17f0b20d154aaf32f257002b7445498c6a9707f
Daniele Tortora <tortora87@gmail.com>
2023-02-07 17:15:18 +0100

    update repo with frontiers

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@myturborepo/ui'],
+  modules: ['@frontiers-fe/ui'],
   nitro: {
     preset: 'netlify',
   },
